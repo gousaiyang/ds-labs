7c5bed9188dac43a9b8050761cd3d424618f2373
gousaiyang <gousaiyang@163.com>
2018-04-01 15:10:24 +0800

    lab2: Adjust parameters

--- lab2/qos.c
+++ lab2/qos.c
@@ -11,8 +11,10 @@
 
 struct rte_meter_srtcm_params srtcm_params[APP_FLOWS_MAX];
 struct rte_meter_srtcm srtcm_data[APP_FLOWS_MAX];
-struct rte_red_config red_params[APP_FLOWS_MAX][e_RTE_METER_COLORS];
+struct rte_red_config red_params[e_RTE_METER_COLORS];
 struct rte_red red_data[APP_FLOWS_MAX][e_RTE_METER_COLORS];
+unsigned queue_size[APP_FLOWS_MAX][e_RTE_METER_COLORS] = {};
+uint64_t last_time = 0;
 
 #define SRTCM_CONFIG(flow_id, cir_, cbs_, ebs_) do { \
     srtcm_params[(flow_id)].cir = (cir_); \
@@ -22,16 +24,14 @@ struct rte_red red_data[APP_FLOWS_MAX][e_RTE_METER_COLORS];
         rte_panic("Cannot init srTCM data\n"); \
 } while (0)
 
-#define RED_CONFIG(flow_id, color, wq_log2, min_th, max_th, maxp_inv) do { \
-    if (rte_red_config_init(&red_params[(flow_id)][(color)], (wq_log2), (min_th), (max_th), (maxp_inv)) != 0) \
+#define RED_CONFIG(color, wq_log2, min_th, max_th, maxp_inv) do { \
+    if (rte_red_config_init(&red_params[(color)], (wq_log2), (min_th), (max_th), (maxp_inv)) != 0) \
         rte_panic("Cannot init RED config\n"); \
-    if (rte_red_rt_data_init(&red_data[(flow_id)][(color)]) != 0) \
-        rte_panic("Cannot init RED data\n"); \
+    int i; \
+    for (i = 0; i < APP_FLOWS_MAX; ++i) \
+        if (rte_red_rt_data_init(&red_data[i][(color)]) != 0) \
+            rte_panic("Cannot init RED data\n"); \
 } while (0)
-
-
-uint64_t last_time = 0;
-unsigned queue_size[APP_FLOWS_MAX] = {};
 
 
 /**
@@ -42,10 +42,10 @@ qos_meter_init(void)
 {
     /* to do */
 
-    SRTCM_CONFIG(0, 2000, 1152, 1152);
-    SRTCM_CONFIG(1, 2000, 640, 640);
-    SRTCM_CONFIG(2, 2000, 384, 384);
-    SRTCM_CONFIG(3, 2000, 256, 256);
+    SRTCM_CONFIG(0, 160000000000, 80000, 80000);
+    SRTCM_CONFIG(1, 80000000000, 40000, 40000);
+    SRTCM_CONFIG(2, 40000000000, 20000, 20000);
+    SRTCM_CONFIG(3, 20000000000, 10000, 10000);
 
     return 0;
 }
@@ -69,18 +69,9 @@ qos_dropper_init(void)
 {
     /* to do */
 
-    RED_CONFIG(0, GREEN, 12, 1022, 1023, 255);
-    RED_CONFIG(0, YELLOW, 12, 1, 2, 1);
-    RED_CONFIG(0, RED, 12, 1, 2, 1);
-    RED_CONFIG(1, GREEN, 12, 1022, 1023, 255);
-    RED_CONFIG(1, YELLOW, 12, 1, 2, 1);
-    RED_CONFIG(1, RED, 12, 1, 2, 1);
-    RED_CONFIG(2, GREEN, 12, 1022, 1023, 255);
-    RED_CONFIG(2, YELLOW, 12, 1, 2, 1);
-    RED_CONFIG(2, RED, 12, 1, 2, 1);
-    RED_CONFIG(3, GREEN, 12, 1022, 1023, 255);
-    RED_CONFIG(3, YELLOW, 12, 1, 2, 1);
-    RED_CONFIG(3, RED, 12, 1, 2, 1);
+    RED_CONFIG(GREEN, 9, 1022, 1023, 10);
+    RED_CONFIG(YELLOW, 9, 1022, 1023, 10);
+    RED_CONFIG(RED, 9, 0, 1, 10);
 
     return 0;
 }
@@ -90,13 +81,22 @@ qos_dropper_run(uint32_t flow_id, enum qos_color color, uint64_t time)
 {
     /* to do */
 
-    if (time != last_time)
-        memset(queue_size, 0, sizeof(queue_size));
+    assert(flow_id < APP_FLOWS_MAX);
 
-    int result = !!rte_red_enqueue(&red_params[flow_id][color], &red_data[flow_id][color], queue_size[flow_id], time);
+    if (time != last_time) {
+        memset(queue_size, 0, sizeof(queue_size));
+        int i, j;
+        for (i = 0; i < APP_FLOWS_MAX; ++i)
+            for (j = 0; j < e_RTE_METER_COLORS; ++j)
+                rte_red_mark_queue_empty(&red_data[i][j], time);
+    }
+
+    last_time = time;
+
+    int result = !!rte_red_enqueue(&red_params[color], &red_data[flow_id][color], queue_size[flow_id][color], time);
 
     if (!result)
-        ++queue_size[flow_id];
+        ++queue_size[flow_id][color];
 
     return result;
 }
